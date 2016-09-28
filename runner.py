7f6125902984bb49e73782d59324f92033266d6e
FelixRiegBaumhauer <friegbaumhauer@stuy.edu>
2016-09-27 21:31:57 -0400

    Beginings

---
+++ runner.py
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#Fxns that are in this file:
+#
+#strlen
+#strcpy/strncpy (choose one)
+#strcat/strncat (choose one)
+#strcmp
+#strchr
+
+#Plan for strlen
+#Interate through, have a counter, at every byte add 1 to count
+#Once you find the null expection, ie 0 or '' return count
+
+
+int main(){
+
+    return 0
+}
