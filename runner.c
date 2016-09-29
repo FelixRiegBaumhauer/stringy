dc2d56c782d76695cfd7b3b37d3a758f1e858582
riegbaumhauer <riegbaumhauer@gmail.com>
2016-09-29 11:44:38 -0400

    nicefy

--- runner.c
+++ runner.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#Fxns that are in this file:
-#
-#strlen
-#strcpy/strncpy (choose one)
-#strcat/strncat (choose one)
-#strcmp
-#strchr
+//Fxns that are in this file:
+//
+//strlen
+//strcpy/strncpy (choose one)
+//strcat/strncat (choose one)
+//strcmp
+//strchr
 
-#Plan for strlen
-#Interate through, have a counter, at every byte add 1 to count
-#Once you find the null expection, ie 0 or '' return count
+//Plan for strlen
+//Interate through, have a counter, at every byte add 1 to count
+//Once you find the null expection, ie 0 or '' return count
 
 
 int main(){
